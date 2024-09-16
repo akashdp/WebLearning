644a0f79e4bdf9eced1fc7180a26b49cee15ee4e
akash GitHub <akashdp50@gmail.com>
2024-09-16 20:58:17 +0530

    Update React component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./components/Home"
 import Project from "./components/Project"
 import About from "./components/About"
 import Contact from "./components/Contact"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 
 
 function App() {
